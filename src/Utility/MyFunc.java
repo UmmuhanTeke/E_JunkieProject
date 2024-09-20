effc684ffd2436abd5edaa28de60ef8c25f005e8
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-09-20 18:49:32 +0300

    Initial commit

---
+++ src/Utility/MyFunc.java
@@ -0,0 +1,25 @@
+package Utility;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+
+public class MyFunc {
+
+    public static void Wait(int second) {
+        try {
+            Thread.sleep(second * 1000l);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void scrollElement(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) BaseDriver.driver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    public static void jsClick(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) BaseDriver.driver;
+        js.executeScript("arguments[0].click();", element);
+    }
+}
